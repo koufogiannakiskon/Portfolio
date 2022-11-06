3972a29e6bed37ea302d95f1f965b85b72f3f2ba
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-06 18:25:50 +0200

    add animation on each row

--- js/fadeOnScrolling.js
+++ js/fadeOnScrolling.js
@@ -1,17 +1,13 @@
 window.addEventListener('scroll', reveal);
 
 function reveal() {
-    var reveals = document.querySelectorAll('.row');
+    var reveals = document.querySelectorAll('.reveal');
     reveals.forEach(row => {
         var windowheight = window.innerHeight;
         var revealtop = row.getBoundingClientRect().top;
         var revealpoint = 150;
         if (revealtop < windowheight - revealpoint) {
             row.classList.add('active');
-        } else {
-            row.classList.remove('active');
-
         }
-
     });
 }
