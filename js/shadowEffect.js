15da4d8eb1e85de1731fa0cb128d1ce1afd6f930
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-10 17:01:59 +0200

    try out shadow effect on mobile

--- js/shadowEffect.js
+++ js/shadowEffect.js
@@ -18,12 +18,27 @@ function shadow(e) {
 
 all_page.addEventListener('mousemove', shadow);
 
+var degtorad = Math.PI / 180;
 
 window.addEventListener('deviceorientation', function (e) {
-    const xWalk = Math.round((walk / 2) * Math.cos(e.alpha));
-    const yWalk = Math.round((walk / 2) * Math.cos(e.beta));
-    my_name.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
+    var _x = e.beta ? e.beta * degtorad : 0;
+    var _y = e.gamma ? e.gamma * degtorad : 0;
+    var _z = e.alpha ? e.alpha * degtorad : 0;
+
+    var cX = Math.cos(_x);
+    var cY = Math.cos(_y);
+    var cZ = Math.cos(_z);
+    var sX = Math.sin(_x);
+    var sY = Math.sin(_y);
+    var sZ = Math.sin(_z);
+
+    // Calculate Vx and Vy components
+    var Vx = - cZ * sY - sZ * sX * cY;
+    var Vy = - sZ * sY + cZ * sX * cY;
+    var x = Math.round((walk / 2) * Vx);
+    var y = Math.round((walk / 2) * Vy);
+    my_name.style.textShadow = `${Vx}px ${Vy}px 2px #7B7A72`
     titles.forEach(title => {
-        title.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
+        title.style.textShadow = `${Vx}px ${Vy}px 2px #7B7A72`
     });
 })
