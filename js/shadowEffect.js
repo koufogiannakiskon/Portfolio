4a199fbc062e56893e3eb3b85f9d0c61a0e9944c
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-10 16:36:25 +0200

    try out shadow effect on mobile

--- js/shadowEffect.js
+++ js/shadowEffect.js
@@ -9,6 +9,7 @@ function shadow(e) {
 
     const xWalk = Math.round((x / width * walk) - (walk / 2));
     const yWalk = Math.round((y / height * walk) - (walk / 2));
+
     my_name.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
     titles.forEach(title => {
         title.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
@@ -18,3 +19,11 @@ function shadow(e) {
 all_page.addEventListener('mousemove', shadow);
 
 
+window.addEventListener('deviceorientation', function (e) {
+    const xWalk = Math.round((walk / 2) * Math.cos(e.alpha));
+    const yWalk = Math.round((walk / 2) * Math.cos(e.beta));
+    my_name.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
+    titles.forEach(title => {
+        title.style.textShadow = `${xWalk}px ${yWalk}px 2px #7B7A72`
+    });
+})
